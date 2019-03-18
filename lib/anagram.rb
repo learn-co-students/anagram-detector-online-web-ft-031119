9604578954e51e44dd280c8ca6ad6454b9dc7f9c
kyle smith <kylemclellan@gmail.com>
2019-03-18 18:43:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :input
+  
+  def initialize(input)
+    @input = input
+  end
+  
+  def match(comp_list)
+    sorted = @input.split("").sort
+    possible = []
+    comp_list.each do |value|
+      maybe = value.split("").sort
+      if maybe == sorted
+        possible << value 
+      end
+    end
+    possible
+  end
+end
